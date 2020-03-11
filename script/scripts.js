04fc8f1023ad962708c84d93cf3e11912f71c1e5
sash5borys <sash5borys@gmail.com>
2020-03-11 18:43:24 +0200

    Made Overlay Mobile Menu

--- script/scripts.js
+++ script/scripts.js
@@ -1,8 +1,19 @@
+// Open|Close Overlay Mobile Menu
+const mobileMenu = document.querySelector('#overlay-mobile-menu');
+const openOverlayMobileMenuButton = document.querySelector('#mobile-menu-open-button');
+const closeOverlayMobileMenuButton = document.querySelector('#mobile-menu-close-button');
+openOverlayMobileMenuButton.addEventListener('click', () => {
+    mobileMenu.style = 'width:65%;';
+});
+closeOverlayMobileMenuButton.addEventListener('click', () => {
+    mobileMenu.style = 'width:0%;';
+});
+
 // Open|Close Overlay Catalog Menu
 const catalogMenu = document.querySelector('.catalog-menu');
-const overlayCatalogOpenButton = document.querySelector('#catalog-menu-open-button');
 const overlayCatalogMenu = document.querySelector('#overlay-catalog-menu');
-overlayCatalogOpenButton.addEventListener('click', () => {
+const openOverlayCatalogButton = document.querySelector('#catalog-menu-open-button');
+openOverlayCatalogButton.addEventListener('click', () => {
     overlayCatalogMenu.style = 'height: auto;';
 });
 document.addEventListener('click', event => {
