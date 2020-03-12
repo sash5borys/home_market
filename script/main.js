3be321fb0e4f8c1a67db52aff295fa0f6e7021f4
sash5borys <sash5borys@gmail.com>
2020-03-12 15:16:48 +0200

    Made Products Carousel

--- script/main.js
+++ script/main.js
@@ -1,1 +1,3 @@
 const carouselSlider = Carousel(document.querySelector('#carousel-slider'));
+const carouselNewProducts = Carousel(document.querySelector('#carousel-new-products'));
+const carouselTrendProducts = Carousel(document.querySelector('#carousel-trend-products'));
