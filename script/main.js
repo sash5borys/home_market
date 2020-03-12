ab182716fff56467e1608c1a3b674068e2437d4a
sash5borys <sash5borys@gmail.com>
2020-03-12 12:45:10 +0200

    Made Slider

---
+++ script/main.js
@@ -0,0 +1,1 @@
+const carouselSlider = Carousel(document.querySelector('#carousel-slider'));
--- script/scripts.js
+++ script/scripts.js
@@ -19,3 +19,108 @@ openOverlayCatalogButton.addEventListener('click', () => {
 document.addEventListener('click', event => {
     if (!catalogMenu.contains(event.target)) overlayCatalogMenu.style = 'height: 0;';
 });
+
+// Carousel
+function Carousel(carousel) {
+    if (!(carousel instanceof Element)) throw new Error('No slider passed in');
+    const carouselContent = carousel.querySelector('.carousel-content');
+    // eslint-disable-next-line prefer-const
+    let countSlides;
+    let initialWidth = 0;
+    let widthOfSlide;
+    let firstSlide;
+    let lastSlide;
+    let firstSlideClone;
+    let lastSlideClone;
+    let canMoving = true;
+
+    const prevButton = carousel.querySelector('.carousel-nav-left');
+    const nextButton = carousel.querySelector('.carousel-nav-right');
+
+    function init() {
+        countSlides = carousel.dataset.countSlides
+            ? carousel.dataset.countSlides
+            : carousel.dataset.minSize
+            ? Math.floor(window.innerWidth / carousel.dataset.minSize)
+            : 1;
+        widthOfSlide = carousel.offsetWidth / countSlides;
+        firstSlide = carouselContent.firstElementChild;
+        lastSlide = carouselContent.lastElementChild;
+        firstSlideClone = firstSlide.cloneNode(true);
+        lastSlideClone = lastSlide.cloneNode(true);
+    }
+
+    function addBefore() {
+        carouselContent.insertBefore(lastSlideClone, firstSlide);
+    }
+
+    function addAfter() {
+        carouselContent.insertBefore(firstSlideClone, lastSlide.nextSibling);
+    }
+
+    function removeBefore() {
+        carouselContent.firstElementChild.remove();
+    }
+
+    function removeAfter() {
+        carouselContent.lastElementChild.remove();
+    }
+
+    // Paste style to every slide
+    function makeSlides() {
+        carouselContent.querySelectorAll('.slide').forEach((el, index) => {
+            el.style.cssText += `width:${widthOfSlide}px;`;
+            el.style.cssText += `background-color:${el.dataset.bgOfSlide};`;
+            if (index == 0) {
+                el.style.cssText += `left:${-widthOfSlide}px;`;
+            } else {
+                el.style.cssText += `left:${initialWidth}px;`;
+                initialWidth += widthOfSlide;
+            }
+            setTimeout(function() {
+                el.style.cssText += 'transition: all .4s;';
+                canMoving = true;
+            }, 1000);
+        });
+        initialWidth = 0;
+    }
+
+    function move(direction) {
+        init();
+        if (canMoving) {
+            canMoving = false;
+            if (direction === 'back') {
+                addBefore();
+                removeAfter();
+            } else if (direction === 'forward') {
+                addAfter();
+                removeBefore();
+            }
+        }
+        makeSlides();
+    }
+
+    init();
+    makeSlides();
+
+    // Initial carousel when window resized
+    window.addEventListener('resize', function() {
+        init();
+        makeSlides();
+    });
+
+    // Navigate event listeners
+    prevButton.addEventListener('click', () => {
+        move('back');
+    });
+    nextButton.addEventListener('click', () => {
+        move('forward');
+    });
+
+    // Make scroll automaticaly
+    if (carousel.classList.contains('slider')) {
+        setInterval(() => {
+            move('forward');
+        }, 5000);
+    }
+}
